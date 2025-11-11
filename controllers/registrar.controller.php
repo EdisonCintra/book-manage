26da3c76ba022b9c6e3e9e7d51379a2dd320ee64
EdisonCintra <edisoncintra6@gmail.com>
2025-11-11 16:50:10 -0300

    Refatoring code

--- controllers/registrar.controller.php
+++ controllers/registrar.controller.php
@@ -16,69 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         $_SESSION['validacoes'] = $validacao->validacoes;
 
-        header("Location: /login");
-
-        exit();
-
-    }
-
-    $validacoes = [];
-
-    $nome = $_POST['nome'];
-
-    $email = $_POST['email'];
-
-    $email_confirmacao = $_POST['email_confirmacao'];
-
-    $senha = $_POST['senha'];
-
-    if (strlen($nome) == 0) {
-
-        $validacoes[] = 'O nome é obrigatório.';
-
-    }
-
-    if (strlen($email) == 0) {
-
-        $validacoes[] = 'O email é obrigatório.';
-
-    }
-
-    if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-        $validacoes[] = 'O email é inválido.';
-
-    }
-
-    if ($email != $email_confirmacao) {
-
-        $validacoes[] = 'O email de confirmação está diferente.';
-
-    }
-
-    if (strlen($senha) == 0) {
-
-        $validacoes[] = 'A senha é obrigatória.';
-
-    }
-
-    if (strlen($senha) < 8 || strlen($senha) > 30) {
-
-        $validacoes[] = 'A senha precisa ter entre 8 e 30 caracteres.';
-
-    }
-
-    if (!str_contains($senha, '*')) {
-
-        $validacoes[] = 'A senha precisa um * nela.';
-
-    }
-
-    if (sizeof($validacoes) > 0) {
-
-        $_SESSION['validacoes'] = $validacoes;
-
-        header("Location: /login");
+        header("Location: /book-manage/login");
 
         exit();
 
@@ -96,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     );
 
-    header('location: /login?mensagem=Registrado com sucesso!');
+    header('location: /book-manage/login?mensagem=Registrado com sucesso!');
 
     exit();
 
