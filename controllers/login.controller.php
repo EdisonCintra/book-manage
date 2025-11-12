1bbc02406d0e93fc23eeb5f4e32fb17a704ee871
EdisonCintra <edisoncintra6@gmail.com>
2025-11-12 15:54:15 -0300

    Method for Auth added

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -2,4 +2,23 @@
 
 $mensagem = $_REQUEST['mensagem'] ?? '';
 
+if( $_SERVER['REQUEST_METHOD'] == 'POST'){
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $usuario = $database->query(
+        query: "SELECT * FROM usuarios WHERE email = :email AND senha = :senha", 
+        params: compact('email', 'senha')
+    )->fetch();
+
+    if($usuario){
+        $_SESSION['auth'] = $usuario;
+        $_SESSION['mensagem'] = 'Seja bem vindo '. $usuario['nome'];
+        header('location: /book-manage');
+        exit();
+    } else {
+        $mensagem = 'Email ou senha incorretos';
+    }
+}
+
 view('login', compact('mensagem'));
